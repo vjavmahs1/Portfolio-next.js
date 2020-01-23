95ccb70f797d1c17862f9c5623391bfb1e8194ad
kimsj1993 <kimsj1993@gmail.com>
2020-01-22 20:35:07 -0600

    added nav bar for each portfoli

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Link from 'next/link'
 import BasePage from '../components/BasePage'
 import BaseLayout from '../components/layouts/BaseLayout'
-import { Col, Row, Card, CardHeader, CardBody, CardText, CardTitle} from 'reactstrap'
+import { Col, Row, Card, CardHeader, CardBody, CardText, CardTitle, Button} from 'reactstrap'
 import {getPortfolios} from '../actions';
 
+import {Router} from '../routes'
 
 class Portfolios extends React.Component {
 
@@ -30,8 +31,14 @@ class Portfolios extends React.Component {
                         <p className="portfolio-card-city"> {portfolio.skills} </p>
                         <CardTitle className="portfolio-card-title">{portfolio.title}</CardTitle>
                         <CardText className="portfolio-card-text">{portfolio.description} {index}</CardText>
-                        <div className="readMore"> </div>
+                        <div className="readMore">
+                        <React.Fragment>
+                        <Button onClick={() => Router.pushRoute(`/portfolio/${portfolio._id}/edit`)} color="warning">Edit</Button> {' '}
+                            <Button color="danger">Delete</Button>
+                        </React.Fragment>
+                        </div>
                         </CardBody>
+
                     </Card>
                     </span>
                 </React.Fragment>
@@ -47,6 +54,8 @@ class Portfolios extends React.Component {
         return (
             <BaseLayout {...this.props.auth}>
                 <BasePage className='portfolio-page' title ="Portfolios">
+                    <Button onClick={() => Router.pushRoute('/portfolioNew')}
+                             color="success" className="create-port-btn">Create Portfolio</Button>
                     <Row>
                         {this.renderPortfolios(portfolios)}
                     </Row>
