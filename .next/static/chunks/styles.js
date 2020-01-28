94ff4d82b4e777d1cbbe6b7b32a1b5ac8f1f50fb
kimsj1993 <kimsj1993@gmail.com>
2020-01-27 22:29:55 -0600

    fixed error for deployed version

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1580183178993");
+            injectCss(link, link.href.split("?")[0] + "?unix=1580185306504");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1580183179010");
+            injectCss(link, link.href.split("?")[0] + "?unix=1580185306982");
           });
       }
     }
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,7 @@
 //For deploy
 
 module.exports = {
-    DB_URI : process.env.DB_URI
+    DB_URI : process.env.DB_URI,
+    NAMESPACE: 'https://seungjin-portfolio.herokuapp.com'
+
 }
