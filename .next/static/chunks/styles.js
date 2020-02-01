7dc4620539b35ccd9ba42ccc085aa3a9e108f8e0
kimsj1993 <kimsj1993@gmail.com>
2020-02-01 01:15:35 -0600

    set the description into lowercase

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -103,7 +103,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1580511639920");
+            injectCss(link, link.href.split("?")[0] + "?unix=1580540464795");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f06539baffe2f61e5930";
+/******/ 	var hotCurrentHash = "c3b7763df2193f137164";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
